35ffcec09f56c17a5416c70e8de81fe3993b6ed2
rajendraacharya1ls <rajendraacharya1ls@gmail.com>
2024-05-21 23:22:57 +0545

    CRUDE Opertaion

---
+++ Dot_net/StudentPortal.Web/Models/AddStudentViewModel.cs
@@ -0,0 +1,11 @@
+namespace StudentPortal.Web.Models
+{
+    public class AddStudentViewModel
+    {
+        public Guid Id { get; set; }
+        public String Name { get; set; }
+        public String Email { get; set; }
+        public String Phone { get; set; }
+        public bool Subscribed { get; set; }
+    }
+}
